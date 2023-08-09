b1fed4906e0044fc6286d775c582c1b4954c3045
Ysamwel <yvonneatieno2000@gmail.com>
2023-08-09 11:35:21 +0000

    c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - it checks if singly linked list has cycle in it.
+ * @list: points to beginning of node
+ * Return: 0 no circle, 1 on cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current, *check;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	current = list;
+	check = current->next;
+
+	while (current != NULL && check->next != NULL
+			&& check->next->next != NULL)
+	{
+		if (current == check)
+			return (1);
+		current = current->next;
+		check = check->next->next;
+	}
+	return (0);
+}
