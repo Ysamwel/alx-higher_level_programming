006a58ee6774a92870d827ead03c049006dcae43
Ysamwel <yvonneatieno2000@gmail.com>
2023-09-07 11:20:45 +0000

    String representation correction

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -7,9 +7,9 @@ Defines a class Rectangle
 class Rectangle:
     """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
-    """Initializes the rectangle"""
-    self.width = width
-    self.height = height
+        """Initializes the rectangle"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
