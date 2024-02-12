59fb16b5709a720316dea51b9ec8a391a4b52011
Ysamwel <yvonneatieno2000@gmail.com>
2024-02-12 21:05:27 +0000

    base class

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,1 @@
+0x0C. Python - Almost a circle. This project tests our understanding on *args, **kwargs, JSON rules and Tests
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''The module of Base class for the rest of classes'''
+
+class Base:
+    '''Representation of the Base class of hierarchy.'''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''class constructor'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+	    self.id = Base.__nb_objects
+	    
+
