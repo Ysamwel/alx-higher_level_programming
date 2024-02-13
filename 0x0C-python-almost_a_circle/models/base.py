c165074afddc58dd99a801779b9c3105be62888c
Ysamwel <yvonneatieno2000@gmail.com>
2024-02-13 00:39:36 +0000

    fixing class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """The module of Base class for the rest of classes."""
 
+
 class Base:
-	
+
     """Representation of the Base class of hierarchy."""
 
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         """constructor"""
         if id is not None:
