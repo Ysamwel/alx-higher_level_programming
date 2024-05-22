930dbfd8c0198b25c6b125996a00bb2f3fc36fdc
ysamwel <yvonneatieno2000@gmail.com>
2024-05-23 02:41:07 +0300

    display the status code of GET

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // Get request status
+
 const request = require('request');
 const url = process.argv[2];
 request.get(url, function (error, response) {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log('code:' + ' ' + response.statusCode);
-	}
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:' + ' ' + response.statusCode);
+  }
 });
