a3065ca4a26dc1fcca44246a0db4d48a10b8d3f7
ysamwel <yvonneatieno2000@gmail.com>
2024-05-23 01:43:25 +0300

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // Read from file
 
- const filesystem = require('fs');
- filesystem.readFile(process.argv[2], 'utf-8',
-   function (error, data) {
-     if (error) {
-       console.log(error);
+ const filesys = require('fs');
+ filesys.readFile(process.argv[2], 'utf-8',
+   function (err, data) {
+     if (err) {
+       console.log(err);
        return;
      }
      console.log(data);
