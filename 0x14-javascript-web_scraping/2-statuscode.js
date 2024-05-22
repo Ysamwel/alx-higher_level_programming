aa0347a9874af8e157c1d560861cb5162a027e29
ysamwel <yvonneatieno2000@gmail.com>
2024-05-23 02:18:39 +0300

    status

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Get request status
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (error, response) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('code:' + ' ' + response.statusCode);
+	}
+});
